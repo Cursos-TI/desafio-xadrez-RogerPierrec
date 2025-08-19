172444aa341594e344dc204496b3a853da967a82
Roger Pierre Cedro de Oliveira <rogerpierrec@icloud.com>
2025-08-19 22:00:12 +0000

    loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 
-int main() {
-    
-    // Número de casas a serem movimentadas
-    int casasTorre = 5;
-    int casasBispo = 5;
-    int casasRainha = 8;
+int main(){
+
+    int torre = 1;
+    int bispo = 1;
 
     //Movimento da torre
     //Simula o movimento da torre 5 casas à direita
-
-    printf("Movimento da Torre:\n");
-    for (int i = 1; i <= casasTorre; i++) {
+    
+    printf("Movimento da torre: \n");
+    while(torre <= 5){
         printf("Direita\n");
+        torre++;
     }
 
     //Movimento do bispo
     //Simula o movimento do bispo 5 casas na diagonal pra cima e direita
-    
-    printf("\nMovimento do Bispo:\n");
-    int i = 1;
-    while (i <= casasBispo) {
-        printf("Cima, Direita\n");
-        i++;
-    }
+
+    printf("\nMovimento do bispo: \n");
+    do
+    {
+        printf("Cima, direita\n");
+        bispo++;
+    } while (bispo <= 5);
 
     //Movimento da rainha
     //Simula o movimento da rainha 8 casas à esqueda
-
-    printf("\nMovimento da Rainha:\n");
-    int j = 1;
-    do {
+    
+    printf("\nMovimento da Rainha: \n");
+    for (int rainha = 1; rainha <= 8; rainha++)
+    {
         printf("Esquerda\n");
-        j++;
-    } while (j <= casasRainha);
+    }
 
+    //Movimento do cavalo
+    //Simula o movimento do cavalo em L duas casas para baixo e uma para esquerda
+
+    printf("\nMovimento do cavalo:\n");
+
+    int movimentoCavalo = 1;
+
+    while (movimentoCavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
+    
+    
     return 0;
 }
